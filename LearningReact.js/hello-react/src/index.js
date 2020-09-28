299673d1268d5fec464305df9edcf36036cb9e9c
unknown <bhavnatahelyani288@gmail.com>
2020-09-28 13:33:11 +0530

    commit

--- LearningReact.js/hello-react/src/index.js
+++ LearningReact.js/hello-react/src/index.js
@@ -2,17 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const lakeList = ["Echo Lake", "Maud Lake", "Cascade Lake"];
-
-function App() {
+function Lake() {
   return (
     <div>
-      Eventual Lakes
+      <h1>Visit Jenny Lake!</h1>
     </div>
   );
 }
 
+function SkiResort() {
+  return (
+    <div>
+      <h1>Visit Jackson Hole Mountain Resort!</h1>
+    </div>
+  );
+}
+
+function App(props) {
+  if(props.season === "summer") {
+    return <Lake />;
+  }
+  return <SkiResort />;
+}
+
 ReactDOM.render(
-  <App />,
+  <App season="anything" />,
   document.getElementById('root')
 );
